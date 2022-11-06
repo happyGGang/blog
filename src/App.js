b2fbd52b22bf0732488da5ad6808935f03dd9afd
happyGGang <joy.gang@dxworks.kr>
2022-11-06 22:48:35 +0900

    [update] 제목 변경 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let [글제목] = useState(["남자코트 추천", "강남우동 맛집", "파이썬 독학"]);
+  let [글제목, 글제목변경] = useState([
+    "남자코트 추천",
+    "강남우동 맛집",
+    "파이썬 독학",
+  ]);
   let [따봉, 따봉변경] = useState(0);
 
   return (
@@ -10,7 +14,15 @@ function App() {
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-      <button>글 수정</button>
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy[0] = "여자코트 추천";
+          글제목변경(copy);
+        }}
+      >
+        글 수정
+      </button>
       <div className="list">
         <h4>
           {글제목[0]}
